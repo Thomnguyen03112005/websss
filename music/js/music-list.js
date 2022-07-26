1cdd7e1c2ef4a246f6ac314b44dfccb6946e0954
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-07-25 19:26:06 -0700

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,395 @@
+let allMusic = [
+  {
+      name: "Harley Bird - Home",
+      artist: "Jordan Scho",
+      img: "music-1",
+      src: "music-1"
+  },
+  {
+      name: "Ikson Anywhere – Ikson",
+      artist: "Audio Library",
+      img: "music-2",
+      src: "music-2"
+  },
+  {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+  },
+  {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+  },
+  {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+  },
+  {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+  },
+  {
+    name: "Stay",
+    artist: "The Kid LAROI, Justin Bieber",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+  name: "Có chắc yêu là đây",
+  artist: "Sơn Tùng MTP",
+  img: "music-8",
+  src: "music-8"
+},
+{
+    name: "Muộn rồi mà sao còn",
+    artist: "Sơn Tùng MTP",
+    img: "muonroi",
+    src: "muonroi"
+  },
+  {
+    name: "Mind Games",
+    artist: "Sickick",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Wished 1001",
+    artist: "Kean Nguyen",
+    img: "music-10",
+    src: "music-10"
+  },
+
+  {
+    name: "Try Again",
+    artist: "디어 (d.ear), 재현 (Jaehyun)",
+    img: "try",
+    src: "try"
+  },
+  {
+    name: "Thank u, next",
+    artist: "Ariana Grande",
+    img: "thank",
+    src: "thank"
+  },
+  {
+    name: "Vài câu nói...",
+    artist: "Grey D",
+    img: "vaicaunoi",
+    src: "vaicaunoi"
+  },
+  {
+    name : 'Đường tôi chở em về',
+    artist: "buitruonglinh",
+    img: "toichoemve",
+    src: "toichoemve"
+  },
+  {
+    name : 'Phải chăng em đã yêu',
+    artist : 'Juky San',
+    img: "phaichang",
+    src: "phaichang"
+  },
+  {
+    name : 'Talking to the moon',
+    artist : 'Bruno Mars',
+    img: "tkinmoon",
+    src: "tkinmoon"
+  },
+    {
+    name : 'Kiss me more',
+    artist : 'Dojat',
+    img: "kissfor",
+    src: "kissfor"
+  },
+  {
+    name : 'You so sexy',
+    artist : 'LikyBo - Kraazy',
+    img : 'youso',
+    src : 'youso'
+  },
+  {
+    name : 'ifly',
+    artist : 'Bazzi',
+    img : 'ifly',
+    src : 'ifly'
+  },
+  {
+    name : 'good 4 u',
+    artist : 'Olivia Rodrigo ',
+    img : 'good4u',
+    src : 'good4u'
+  },
+  {
+    name : 'Có hẹn với thanh xuân',
+    artist : 'Monstar',
+    img : 'cohen',
+    src : 'cohen'
+  },
+  {
+    name : 'Mama boy',
+    artist : 'amee',
+    img : 'mmboy',
+    src : 'mmboy'
+  },
+  {
+    name : 'Play date',
+    artist : 'Melanie Martinez',
+    img : 'playdate',
+    src : 'playdate'
+  },
+  {
+    name : 'Rót',
+    artist : 'híu',
+    img : 'rothiu',
+    src : 'rothiu'
+  },
+  {
+    name : 'LoverBoy',
+    artist : 'A-Wall',
+    img : 'loveboy',
+    src : 'loveboy'
+
+  },
+  {
+    name : 'Bored',
+    artist : 'Billie Eilish',
+    img : 'Bored',
+    src : 'Bored'
+
+  },
+  {
+    name : 'Double take',
+    artist : 'dhruv',
+    img : 'double',
+    src : 'double'
+
+  },
+  {
+    name : "always, i'll care",
+    artist : 'Jeremy Zucker',
+    img : 'always',
+    src : 'always'
+
+  },
+  {
+    name : 'infinity',
+    artist : 'James Young',
+    img : 'Infinity',
+    src : 'Infinity'
+  },
+  {
+    name : 'Dusk Till Dawn',
+    artist : 'ZAYN & Sia',
+    img : 'aibiet',
+    src : 'aibiet'
+
+  },
+  {
+    name : 'Thay mọi cô gái yêu anh',
+    artist : 'Amee',
+    img : 'thayamee',
+    src : 'thayamee'
+
+  },
+  {
+    name : 'Everything i wanted',
+    artist : 'Billie Eilish',
+    img : 'eiwant',
+    src : 'eiwant',
+
+  },
+  {
+    name : 'em không hiểu',
+    artist : 'Chang , Minh huy',
+    img : 'emkhonghieu',
+    src : 'emkhonghieu',
+  },
+  {
+    name : 'Để bầu trời nhìn hai ta',
+    artist : 'Hieuthuhai',
+    img : 'bautroihieuthu3',
+    src : 'bautroihieuthu3',
+  },
+  {
+    name : 'Precious',
+    artist : 'DADUC',
+    img : 'precious',
+    src : 'precious',
+
+  },
+  {
+    name : 'Just Friends',
+    artist : 'Audrey Mika',
+    img : 'JustF',
+    src : 'JustF',
+  },
+  {
+    name : 'HONESTY',
+    artist : '(Cover BANG YE DAM - 방예담)',
+    img : 'Hones',
+    src : 'Hones',
+
+  },
+  {
+    name : 'Dịu dàng em đến',
+    artist : 'Erik',
+    img : 'diudangem',
+    src : 'diudangem',
+
+  },
+  {
+    name : 'necessary girl',
+    artist : 'oceanfromtheblue',
+    img : 'oceanfromtheblue',
+    src : 'oceanfromtheblue',
+
+  },
+  {
+    name : 'You Right',
+    artist : 'Doja Cat',
+    img : 'youphai',
+    src : 'youphai',
+  },
+  {
+    name : 'You and I',
+    artist : 'Katy Perry',
+    img : 'banandtoi',
+    src : 'banandtoi',
+
+  },
+  {
+    name : 'Akuma no ko',
+    artist : '「悪魔の子」by Ai Higuchi',
+    img : '',
+    src : 'Akuma',
+
+  },
+  {
+    name : 'Love me like that',
+    artist : 'sam kim',
+    img : 'loveme',
+    src : 'loveme',
+
+  },
+  {
+    name : 'Drunk',
+    artist : 'kean',
+    img : 'drunk',
+    src : 'drunk',
+
+  },
+  {
+    name : 'Đâu đâu cũng là em',
+    artist : 'Yongcaptain',
+    img : 'daucung',
+    src : 'daucung',
+
+  },
+  {
+    name : 'Em có nghe',
+    artist : 'Kha',
+    img : 'emconghe',
+    src : 'emconghe',
+
+  },
+  {
+    name : 'Rolling',
+    artist : 'Richie D. ICY & AXTROBOY',
+    img : 'rolling',
+    src : 'rolling',
+  },
+    {
+      name: "I love you so",
+      artist: "The blue",
+      img: "iloveu",
+      src: "iloveu"
+  },
+      {
+      name: "Khoảng cách",
+      artist: "The Cassette",
+      img: "catsad",
+      src: "catsad"
+  },
+        {
+      name: "TREASURE",
+      artist: "DARARI",
+      img: "treasure",
+      src: "treasure"
+  },
+          {
+      name: "Love Me",
+      artist: "RealestK",
+      img: "loveme",
+      src: "lovemee"
+  },
+            {
+      name: "Say so",
+      artist: "Doja cat",
+      img: "",
+      src: "sayso"
+  },
+            {
+      name: "like that",
+      artist: "Doja cat",
+      img: "",
+      src: "likethat"
+  },
+            {
+      name: "Plastic",
+      artist: "sakehands",
+      img: "",
+      src: "plastic"
+  },
+            {
+      name: "Tabun",
+      artist: "Yoasobi",
+      img: "",
+      src: "tabun"
+  },
+      {
+      name: "Because I Love You",
+      artist: "Yuika",
+      img: "",
+      src: "BecausILoveYou"
+  },
+              {
+      name: "The way that you see me",
+      artist: "Powfu",
+      img: "",
+      src: "powfuthewaythatyouseeme"
+  },
+              {
+      name: "Summertime",
+      artist: "Cinnamons x Evening Cinema",
+      img: "",
+      src: "summertime"
+  },
+              {
+      name: "Better now",
+      artist: "betternow",
+      img: "",
+      src: "betternow"
+  },
+              {
+      name: "love scenario",
+      artist: "ikon",
+      img: "",
+      src: "lovescre"
+  },
+              {
+      name: "Polaroid Love",
+      artist: "ENHYPEN",
+      img: "",
+      src: "PolaroidLove"
+  },
+];
+
+
+
